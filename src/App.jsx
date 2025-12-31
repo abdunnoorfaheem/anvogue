2a0ddf37ece310197639a3d6d67ac03374ac6187
abdunnoorfaheem <abdunnoor3030@gmail.com>
2025-12-31 20:23:13 +0600

    react router dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import RootLayout from './components/layouts/RootLayout'
+import Home from './components/pages/Home'
+import Shop from './components/pages/Shop'
 
 function App() {
  
 
   return (
     <>
-    <h1>anvogue</h1>
+    <Routes>
+      <Route path='/' element={<RootLayout/>}>
+      <Route index element={<Home/>}/>   
+      <Route path='/shop' element={<Shop/>}/>   
+      </Route>
+    </Routes>
     </>
   )
 }
